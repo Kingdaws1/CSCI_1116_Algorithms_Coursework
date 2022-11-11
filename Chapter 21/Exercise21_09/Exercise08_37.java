07ac86fbf72e6a2c73be7ff3ff3f46f5aaa3eb39
Kingdaws1 <107580682+Kingdaws1@users.noreply.github.com>
2022-11-11 14:02:38 -0700

    Add files via upload

---
+++ Chapter 21/Exercise21_09/Exercise08_37.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+public class Exercise08_37 {
+  public static void main(String[] args) {
+    String[][] stateCapital = {
+      {"Alabama", "Montgomery"},
+      {"Alaska", "Juneau"},
+      {"Arizona", "Phoenix"},
+      {"Arkansas", "Little Rock"},
+      {"California", "Sacramento"},
+      {"Colorado", "Denver"},
+      {"Connecticut", "Hartford"},
+      {"Delaware", "Dover"},
+      {"Florida", "Tallahassee"},
+      {"Georgia", "Atlanta"},
+      {"Hawaii", "Honolulu"},
+      {"Idaho", "Boise"},
+      {"Illinois", "Springfield"},
+      {"Indiana", "Indianapolis"},
+      {"Iowa", "Des Moines"},
+      {"Kansas", "Topeka"},
+      {"Kentucky", "Frankfort"},
+      {"Louisiana", "Baton Rouge"},
+      {"Maine", "Augusta"},
+      {"Maryland", "Annapolis"},
+      {"Massachusettes", "Boston"},
+      {"Michigan", "Lansing"},
+      {"Minnesota", "Saint Paul"},
+      {"Mississippi", "Jackson"},
+      {"Missouri", "Jefferson City"},
+      {"Montana", "Helena"},
+      {"Nebraska", "Lincoln"},
+      {"Nevada", "Carson City"},
+      {"New Hampshire", "Concord"},
+      {"New Jersey", "Trenton"},
+      {"New York", "Albany"},
+      {"New Mexico", "Santa Fe"},
+      {"North Carolina", "Raleigh"},
+      {"North Dakota", "Bismarck"},
+      {"Ohio", "Columbus"},
+      {"Oklahoma", "Oklahoma City"},
+      {"Oregon", "Salem"},
+      {"Pennsylvania", "Harrisburg"},
+      {"Rhode Island", "Providence"},
+      {"South Carolina", "Columbia"},
+      {"South Dakota", "Pierre"},
+      {"Tennessee", "Nashville"},
+      {"Texas", "Austin"},
+      {"Utah", "Salt Lake City"},
+      {"Vermont", "Montpelier"},
+      {"Virginia", "Richmond"},
+      {"Washington", "Olympia"},
+      {"West Virginia", "Charleston"},
+      {"Wisconsin", "Madison"},
+      {"Wyoming", "Cheyenne"}
+    };
+    
+    Map<String, String> map = new TreeMap<>();
+    for (int i = 0; i < stateCapital.length; i++) {
+    	map.put(stateCapital[i][0], stateCapital[i][1]);
+    }
+
+    Scanner input = new Scanner(System.in);
+
+    while (true) {
+    	System.out.print("Enter a State to learn its Capital: ");
+    	String state = input.next();
+    	if (map.containsKey(state)) {
+    		System.out.println("The Capital is " + map.get(state));
+    	}
+    	else {
+    		System.out.println("Make sure you spelled the state correctly (include capital letters.)");
+    	}
+    }
+
+  }
+}
